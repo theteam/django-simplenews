b14e90dd9b29dd9bff8ca97122a85646848b3ef4
Alfredo <alfredoaguirre@theteam.co.uk>
2011-04-04 14:55:35 +0100

    Added template as an optional argument to the template tag

--- src/simplenews/templatetags/simplenews_tags.py
+++ src/simplenews/templatetags/simplenews_tags.py
@@ -1,11 +1,21 @@
 from django import template
+from django.template import RequestContext
+from django.template.loader import render_to_string
 
+from templatetag_sugar.register import tag
+from templatetag_sugar.parser import Constant, Optional, Variable
 from simplenews.models import Article
 
 register = template.Library()
 
-@register.inclusion_tag('simplenews/shortlist.html', takes_context=True)
-def render_latest_news(context):
-    """Renders the latest news"""
+@tag(register, [Optional([Constant("with"), Variable()])])
+def render_latest_news(context, template='simplenews/shortlist.html'):
+    """Renders the latest news
+    Takes: "with TEMPLATE_NAME" as an optional parameter
+    {% render_latest_news with "simplenews/shortlist.html" %}
+    """
+    request = context.get('request')
     object_list = Article.live.all().order_by('-created')[:5]
-    return {'object_list': object_list}
+    extra_context = {'object_list': object_list}
+    template_context = RequestContext(request, extra_context)
+    return render_to_string(template, template_context)
